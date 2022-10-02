48b280623d1fc8f5461279b1566dbe766b0fedd3
sherylynn <352281674@qq.com>
2022-10-02 19:47:50 +0800

    change

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
-aapt p -f -M ./AndroidManifest.xml -I /system/framework/framework-res.apk -S ./res/ -F ./unsigned.apk 
-./addon/zipsigner ./unsigned.apk ./signed.apk
-cp ./signed.apk ./magisk/system/product/overlay/
+cd framework
+aapt p -f -M ./AndroidManifest.xml -I /vendor/overlay/framework-res__auto_generated_rro_vendor.apk -S ./res/ -F ./unsigned_framework.apk 
+../addon/zipsigner ./unsigned_framework.apk ./signed_framework.apk
+cp ./signed_framework.apk ../magisk/system/product/overlay/
+rm signed_framework.apk
+rm unsigned_framework.apk
+
+cd ../system_ui
+aapt p -f -M ./AndroidManifest.xml -I /vendor/overlay/SystemUI__auto_generated_rro_vendor.apk  -S ./res/ -F ./unsigned_system_ui.apk 
+../addon/zipsigner ./unsigned_system_ui.apk ./signed_system_ui.apk
+cp ./signed_system_ui.apk ../magisk/system/product/overlay/
+rm signed_system_ui.apk
+rm unsigned_system_ui.apk
+
+cd ../
 cd magisk
 zip -r ../magisk.zip ./
-rm ./../signed.apk
-rm ./../unsigned.apk
+cp ../magisk.zip ~/storage/downloads/
